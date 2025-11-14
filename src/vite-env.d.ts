ba49652fecf45b9788c76830e07b5765fbbc9423
Yenreh <qherney@gmail.com>
2025-11-14 17:39:52 -0500

    feat: add TypeScript declaration for CSS modules in vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -9,3 +9,8 @@ interface ImportMetaEnv {
 interface ImportMeta {
   readonly env: ImportMetaEnv
 }
+
+declare module '*.css' {
+  const content: Record<string, string>;
+  export default content;
+}
